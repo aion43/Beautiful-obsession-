174e1b0d840a964bcda570fa806d4f4debb4a197
aion43 <7.2596362e+07+aion43@users.noreply.github.com>
2026-08-19 09:41:55 +0000

    Publish authorised download links and transfer guidance

--- github-pages/site-config.js
+++ github-pages/site-config.js
@@ -1,10 +1,10 @@
 window.__BEAUTIFUL_OBSESSION_CONFIG__ = {
   links: [
-    { service: "mega", url: "", isEnabled: false },
-    { service: "megaMirror", url: "", isEnabled: false },
-    { service: "drive", url: "", isEnabled: false },
-    { service: "telegram", url: "", isEnabled: false },
-    { service: "torrent", url: "", isEnabled: false },
-    { service: "mediafire", url: "", isEnabled: false }
+    { service: "mega", url: "https://mega.nz/folder/gt0EnTTY#RAdBW4nsrwM77R6JQmWf5w", isEnabled: true },
+    { service: "megaMirror", url: "https://transfer.it/t/e0Gv1l9HyJIA", isEnabled: true },
+    { service: "drive", url: "https://drive.google.com/drive/folders/1-EuASEhM_azkMwKKpo6ZrlpMbQMb2EBF?usp=sharing", isEnabled: true },
+    { service: "telegram", url: "https://t.me/CityzenswingBangladesh", isEnabled: true },
+    { service: "torrent", url: "https://ext.to/browse/?q=a+beautiful+obsession", isEnabled: true },
+    { service: "mediafire", url: "", isEnabled: false },
   ]
 };
