bc6b04bd5c3c379bd41e13febfd490681ae0ba75
aion43 <7.2596362e+07+aion43@users.noreply.github.com>
2026-08-19 09:44:35 +0000

    Activate MediaFire download folder

--- github-pages/site-config.js
+++ github-pages/site-config.js
@@ -5,6 +5,6 @@ window.__BEAUTIFUL_OBSESSION_CONFIG__ = {
     { service: "drive", url: "https://drive.google.com/drive/folders/1-EuASEhM_azkMwKKpo6ZrlpMbQMb2EBF?usp=sharing", isEnabled: true },
     { service: "telegram", url: "https://t.me/CityzenswingBangladesh", isEnabled: true },
     { service: "torrent", url: "https://ext.to/browse/?q=a+beautiful+obsession", isEnabled: true },
-    { service: "mediafire", url: "", isEnabled: false },
+    { service: "mediafire", url: "https://www.mediafire.com/folder/9zv1arvvbyg5q/A+Beautiful+Obsession", isEnabled: true },
   ]
 };
